cd16e1ba947112db145055290f0bac8ea4e5117b
Louise Poubel <louise@openrobotics.org>
2021-07-27 11:28:07 -0700

    debug
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-set -eu
+set -x
+set -e
 
+echo ::group::Install tools
 apt update
 apt install -y git
+echo ::endgroup::
 
+echo "$GITHUB_WORKSPACE"
 cd "$GITHUB_WORKSPACE"
 
+echo ::group::Checkout
 git fetch
 git checkout ${INPUT_FROM_BRANCH}
 git checkout ${INPUT_TO_BRANCH}
 git checkout -b port_${INPUT_FROM_BRANCH}_${INPUT_TO_BRANCH}_`date '+%Y-%m-%d'`
+echo ::endgroup::
+
+echo ::group::Merge
 git merge ${INPUT_FROM_BRANCH}
 git add --all
+echo ::endgroup::
+
+echo ::group::Commit and push
 git commit -c user.name="osrf-triage" -c user.email="sim@openrobotics.org" \
     --author="osrf-triage" \
     -sam"${INPUT_FROM_BRANCH} ➡️  ${INPUT_TO_BRANCH}"
 git push
+echo ::endgroup::
